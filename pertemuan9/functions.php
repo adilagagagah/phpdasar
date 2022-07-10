ad4757a6cf811e3bf8b75f1ac07de94cbfe575d6
gagah adilaga <gagahadilaga6124@gmail.com>
2022-07-10 18:44:20 +0700

    phpdasar day2: query in php

---
+++ pertemuan9/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// koneksi ke database 
+$db = mysqli_connect("localhost", "root", "", "phpdasar");
+
+
+function query($query)
+{
+    global $db;
+    $result = mysqli_query($db, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
