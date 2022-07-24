bba2fb1e037fb10af87d61e572eef710be740de6
gagah <gagahadilaga6124@gmail.com>
2022-07-25 00:03:38 +0700

    pertemuan 14 end

--- pertemuan14/functions.php
+++ pertemuan14/functions.php
@@ -124,3 +124,39 @@ function cari($keyword) {
     return query($query);
 }
 
+function registrasi($data) {
+    global $db;
+    
+    $username = strtolower(stripslashes($data['username']));
+    $password = mysqli_real_escape_string($db, $data['password']);
+    $password2 = mysqli_real_escape_string($db, $data['password2']);
+
+    //cek username sudah ada atau belum
+    $result = mysqli_query($db, "SELECT username FROM user WHERE username = '$username'");
+    // var_dump($result); die;
+    if (mysqli_fetch_assoc($result)) {
+        echo " 
+            <script>
+                alert('tidak bisa ditambahkan, username sudah ada!')
+            </script>";
+        return false;
+    }
+    
+    //cek konfirmasi password
+    if ( $password != $password2) {
+        echo " 
+            <script>
+                alert('konfirmasi password tidak sesuai!')
+            </script>";
+        return false;
+    }
+
+    // enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    
+    // tambahkan user baru ke database
+    mysqli_query($db, "INSERT INTO user VALUES('', '$username', '$password')");
+    
+    return mysqli_affected_rows($db);
+    
+}
