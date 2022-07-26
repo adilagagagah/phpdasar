50dc4e37e715398321c4a1e8169c23c481a09fdb
gagah <gagahadilaga6124@gmail.com>
2022-07-27 00:45:17 +0700

    pertemuan 18 end : pagination (bug not fixed)

--- pertemuan18/index.php
+++ pertemuan18/index.php
@@ -8,12 +8,22 @@ if(!isset($_SESSION['login'])) {
 
 require 'functions.php';
 
-$mahasiswa = query("SELECT * FROM mahasiswa");
+// konfigurasi
+$jumlahDataPerHalaman = 3;
+$jumlahData = count(query("SELECT *  FROM mahasiswa"));
+$jumlahHalaman = ceil($jumlahData/$jumlahDataPerHalaman);
+$halamanAktif = isset($_GET['halaman']) ? $_GET['halaman'] : 1;
+// hal 1, 3 dimulai dari 0
+// hal 2, 3 dimulai dari 3
+// hal 3, 3 dimulai dari 6
+$dataAwal = ($jumlahDataPerHalaman * $halamanAktif) - $jumlahDataPerHalaman;
+
+$mahasiswa = query("SELECT * FROM mahasiswa LIMIT $dataAwal, $jumlahDataPerHalaman"); // #limit >> arg index pertama, arg berapa banyak data dari index pertama
 
 //tombol cari di klik 
 if( isset($_POST["cari"])) {
     $mahasiswa = cari($_POST["keyword"]);
-};
+}
 
 ?>
 
@@ -48,6 +58,26 @@ if( isset($_POST["cari"])) {
     
     <br><br>
 
+    <!-- membuat bar navigasi -->
+    <?php if($halamanAktif > 1): ?>
+        <a href="?halaman=<?php echo ($halamanAktif - 1) ?>">&laquo</a>
+    <?php endif; ?>
+
+    <?php for($i = 1; $i <= $jumlahDataPerHalaman; $i++): ?>
+        <?php if($i == $halamanAktif): ?>
+            <a href="?halaman=<?php echo $i?>" style="font-weight: bold; color: red;"><?php echo $i ?></a>
+        <?php else: ?>
+            <a href="?halaman=<?php echo $i?>"><?php echo $i ?></a>
+        <?php endif; ?>
+    <?php endfor; ?>
+
+    <?php if($halamanAktif < $jumlahHalaman): ?>
+        <a href="?halaman=<?php echo ($halamanAktif + 1) ?>">&raquo</a>
+    <?php endif; ?>
+
+    <br>
+
+    <!-- membuat tabel daftar mahasiswa -->
     <table border="1" cellpadding="10" cellspacing="0">
 
         <tr>
