67e3de15432ab5628be75f295486ea141c2642ed
gagah <gagahadilaga6124@gmail.com>
2022-07-27 04:21:02 +0700

    pertemuan 19 end : live search (ajax)

--- pertemuan19/index.php
+++ pertemuan19/index.php
@@ -41,44 +41,51 @@ if( isset($_POST["cari"])) {
 
         <input type="text" name="keyword" size=40 autofocus 
         placeholder="masukkan pencarian.." autocomplete="off" 
-        id="">
-        <button type="submit" name="cari">cari!</button>
+        id="keyword">
+        <button type="submit" name="cari" id="tombol-cari">cari!</button>
 
     </form>
     
     <br><br>
 
-    <table border="1" cellpadding="10" cellspacing="0">
+    <div id="container">
 
-        <tr>
-            <th>NO.</th>
-            <th>Aksi</th>
-            <th>Gambar</th>
-            <th>NRP</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Jurusan</th>
-        </tr>
+        <table border="1" cellpadding="10" cellspacing="0">
 
-        <?php $id = 1; ?>
-        <?php foreach ($mahasiswa as $row) : ?>
             <tr>
-                <td><?php echo $id; ?></td>
-                <td>
-                    <a href="ubah.php?id=<?php echo $row["id"]; ?>">ubah</a> |
-                    <a href="hapus.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
-                </td>
-                <td><img src="img/<?php echo $row["gambar"] ?>" width="50" alt=""></td>
-                <td><?php echo $row["nrp"] ?></td>
-                <td><?php echo $row["nama"] ?></td>
-                <td><?php echo $row["email"] ?></td>
-                <td><?php echo $row["jurusan"] ?></td>
+                <th>NO.</th>
+                <th>Aksi</th>
+                <th>Gambar</th>
+                <th>NRP</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Jurusan</th>
             </tr>
-            <?php $id++; ?>
-        <?php endforeach; ?>
 
-    </table>
+            <?php $id = 1; ?>
+            <?php foreach ($mahasiswa as $row) : ?>
+                <tr>
+                    <td><?php echo $id; ?></td>
+                    <td>
+                        <a href="ubah.php?id=<?php echo $row["id"]; ?>">ubah</a> |
+                        <a href="hapus.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
+                    </td>
+                    <td><img src="img/<?php echo $row["gambar"] ?>" width="50" alt=""></td>
+                    <td><?php echo $row["nrp"] ?></td>
+                    <td><?php echo $row["nama"] ?></td>
+                    <td><?php echo $row["email"] ?></td>
+                    <td><?php echo $row["jurusan"] ?></td>
+                </tr>
+                <?php $id++; ?>
+            <?php endforeach; ?>
 
+        </table>
+
+    </div>
+
+    <script src="js/script.js">
+
+    </script>
 
 </body>
 
