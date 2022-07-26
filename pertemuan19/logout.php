5bc383cccbe51400668c549fa731185587f9fb43
gagah <gagahadilaga6124@gmail.com>
2022-07-27 03:31:50 +0700

    pertemuan 19 start

---
+++ pertemuan19/logout.php
@@ -0,0 +1,25 @@
+<?php 
+
+session_start();
+
+// hapus session
+// hilangkan session (cara 1) : bikin global variabel session jadi array kosong
+$_SESSION = [];
+
+// hilangkan session (cara 2)
+session_unset();
+
+// hilangkan session (cara 3)
+session_destroy();
+
+
+// hapus cookie
+setcookie('id', '', time() - 3600);
+setcookie('key', '', time() - 3600);
+
+
+// setelah session dan cookie terhapus, kembali ke halaman login
+header("Location: login.php");
+exit;
+
+?>
